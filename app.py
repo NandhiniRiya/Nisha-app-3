a65ae29e472fa80467196e628f5e1922078aa00c
NandhiniRiya <76508660+NandhiniRiya@users.noreply.github.com>
2021-02-26 17:09:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import dash_html_components as html
 app = dash.Dash()
 app.layout = html.Div([
     html.H1('Sam Analytics'),
-    html.Iframe(id='map', srcDoc=open('Rani_Parties (1).html', 'r').read(), width='100%', height='600'),
+    html.Iframe(id='map', srcDoc=open('https://raw.githubusercontent.com/NandhiniRiya/Nisha-app-3/main/Rani_Parties%20(1).html', 'r').read(), width='100%', height='600'),
     html.Button(id='map-submit-button', n_clicks=0, children='Submit')
 ])
 @app.callback(
@@ -13,6 +13,6 @@ def update_map(n_clicks):
     if n_clicks is None:
         return dash.no_update
     else:
-        return open('Rani_Parties (1).html', 'r').read()
+        return open('https://raw.githubusercontent.com/NandhiniRiya/Nisha-app-3/main/Rani_Parties%20(1).html', 'r').read()
 if __name__ == '__main__':
     app.run_server(debug=True, use_reloader=False)
